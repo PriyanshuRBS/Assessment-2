857b0917ee1662ea6b2313008a4556017994f345
Priyanshu Shriv <Priyanshu.shriv@rbslearn.net>
2025-06-23 17:33:04 +1000

    test

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ while dead == False:
 
             print("What will you fight with? You have:")
             for a in range(len(bag)):                       # show what’s in the bag
-                print(item.name)
+                print(a+1, item.name)
 
             choice = input("> ").strip()
 
