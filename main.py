1fedb1753e96c8b91dbc8c238c083d6bebbcb22d
Priyanshu Shriv <Priyanshu.shriv@rbslearn.net>
2025-06-23 20:30:25 +1000

    another attempt to fix

--- main.py
+++ main.py
@@ -226,6 +226,7 @@ while dead == False:
                 if choice == bag[b-1].name:
                     print('You have that')
                     choice == bag[b-1]
+                    print(choice.name)
 
         # ---------- FIGHT ----------
         # fight returns the *updated* health & dead flag
